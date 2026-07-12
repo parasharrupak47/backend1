fec112a588e5651ff21e7e2b3d43e3b0374ed73f
parasharrupak47 <rupakp.ug23.cs@nitp.ac.in>
2026-07-12 17:00:33 +0530

    handling of edge case when cover image is not uploaded line number 40-44

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -35,7 +35,13 @@ const registerUser = asynchandler(async (req, res) => {
 
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
 
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  let coverImageLocalPath = null;
+
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
